3d0d3ac1730111c581a775207667040deda38510
Yurii Mironov <yuriymironov96@gmail.com>
2025-11-23 00:44:35 +0200

    feat: basic ci/cd

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -18,24 +18,24 @@ async def create_user(*, session: AsyncSession, user_create: UserCreate) -> User
     return db_obj
 
 
-async def update_user(*, session: AsyncSession, db_user: User, user_in: UserUpdate) -> Any:
-    user_data = user_in.model_dump(exclude_unset=True)
-    extra_data = {}
-    if "password" in user_data:
-        password = user_data["password"]
-        hashed_password = get_password_hash(password)
-        extra_data["hashed_password"] = hashed_password
-    db_user.sqlmodel_update(user_data, update=extra_data)
-    session.add(db_user)
-    await session.commit()
-    await session.refresh(db_user)
-    return db_user
+# async def update_user(*, session: AsyncSession, db_user: User, user_in: UserUpdate) -> Any:
+#     user_data = user_in.model_dump(exclude_unset=True)
+#     extra_data = {}
+#     if "password" in user_data:
+#         password = user_data["password"]
+#         hashed_password = get_password_hash(password)
+#         extra_data["hashed_password"] = hashed_password
+#     db_user.sqlmodel_update(user_data, update=extra_data)
+#     session.add(db_user)
+#     await session.commit()
+#     await session.refresh(db_user)
+#     return db_user
 
 
 async def get_user_by_username(*, session: AsyncSession, username: str) -> User | None:
     statement = select(User).where(User.username == username)
-    session_user = await session.execute(statement)
-    session_user = session_user.scalar_one_or_none()
+    result = await session.execute(statement)
+    session_user = result.scalar_one_or_none()
     return session_user
 
 
